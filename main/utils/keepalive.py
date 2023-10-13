57015bee82cc3aa9b250ad74482cd0f739b5a985
Karthi-Villain <69628712+Karthi-Villain@users.noreply.github.com>
2023-10-13 21:16:46 +0530

    Update keepalive.py

--- main/utils/keepalive.py
+++ main/utils/keepalive.py
@@ -8,14 +8,14 @@ from main import Var
 
 
 async def ping_server():
-    sleep_time = Var.PING_INTERVAL
+    sleep_time = 600
     while True:
         await asyncio.sleep(sleep_time)
         try:
             async with aiohttp.ClientSession(
                 timeout=aiohttp.ClientTimeout(total=10)
             ) as session:
-                async with session.get(Var.URL) as resp:
+                async with session.get(Var.APP_URL) as resp:
                     logging.info("Pinged server with response: {}".format(resp.status))
         except TimeoutError:
             logging.warning("Couldn't connect to the site URL..!")
